6869798774dce08e41be458af48c4b667d832e0e
ikkkp <2496450092@qq.com>
2023-10-08 11:21:08 +0800

    modified:   packages/runtime-core/src/createApp.ts

--- packages/runtime-core/src/createApp.ts
+++ packages/runtime-core/src/createApp.ts
@@ -8,8 +8,6 @@ export function createAppAPI(render) {
         console.log("基于根组件创建 vnode");
         const vnode = createVNode(rootComponent);
         console.log("调用 render，基于 vnode 进行开箱");
-        //
-        //
         render(vnode, rootContainer);
       },
     };
