632798bcb868f5f5b894f787de12b37fea91f88a
ikkkp <2496450092@qq.com>
2023-10-09 16:43:22 +0800

    modified:   packages/compile-core/src/compile.ts

--- packages/compile-core/src/compile.ts
+++ packages/compile-core/src/compile.ts
@@ -5,6 +5,13 @@ import { transformExpression } from "./transforms/transformExpression";
 import { transformElement } from "./transforms/transformElement";
 import { transformText } from "./transforms/transformText";
 
+/**
+* @Description:template模板编译
+* @Version:1.0
+* @Author:Huangzl
+* @Date:2023/10/09 16:42:45
+*/
+
 export function baseCompile(template, options) {
   // 1. 先把 template 也就是字符串 parse 成 ast
   const ast = baseParse(template);
