42d670967cb17b5562d066f3db4bbf98eacee1f2
ikkkp <2496450092@qq.com>
2023-10-09 17:29:04 +0800

    modified:   packages/compile-core/src/transform.ts

--- packages/compile-core/src/transform.ts
+++ packages/compile-core/src/transform.ts
@@ -14,6 +14,7 @@ export function transform(root, options = {}) {
   root.helpers.push(...context.helpers.keys());
 }
 
+
 function traverseNode(node: any, context) {
   const type: NodeTypes = node.type;
 
